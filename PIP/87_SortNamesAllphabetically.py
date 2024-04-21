d5baba476d6744c0c26f9ab633cd74fc44306952
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-04-21 16:46:53 +0530

    Another One

---
+++ PIP/87_SortNamesAllphabetically.py
@@ -0,0 +1,13 @@
+def bubble_sort(names):
+    n = len(names)
+    for i in range(0,n) :
+        for j in range(0,n-i-1) :
+            if names[j] > names[j+1] :
+                temp = names[j]
+                names[j] = names[j+1]
+                names[j+1] = temp
+
+names = ["John", "Alice", "Bob", "Charlie", "Emma"]
+print("Original list:", names)
+bubble_sort(names)
+print("Sorted list:", names)
