da9425000a38e6a5887997ec4769198afcd420a4
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-06-10 11:02:28 +0530

    blah blah

--- PIP/09_InputOutput.py
+++ PIP/09_InputOutput.py
@@ -1,7 +1,17 @@
+# input_file = r"C:\Users\Asus\Downloads\Study\College\Codes\6th_Sem\PIP\Input.txt"
+# with open(input_file, 'r') as f1:
+#     data = f1.read()
+# output_file = r"C:\Users\Asus\Downloads\Study\College\Codes\6th_Sem\PIP\Output.txt"
+# with open(output_file, 'w') as f2:
+#     convertedData = data.upper()
+#     f2.write(convertedData)
+
 input_file = r"C:\Users\Asus\Downloads\Study\College\Codes\6th_Sem\PIP\Input.txt"
-with open(input_file, 'r') as f1:
-    data = f1.read()
+f1 = open(input_file,'r')
+data = f1.read()
+f1.close()
 output_file = r"C:\Users\Asus\Downloads\Study\College\Codes\6th_Sem\PIP\Output.txt"
-with open(output_file, 'w') as f2:
-    convertedData = data.upper()
-    f2.write(convertedData)
+f2 = open(output_file,'w')
+convertedData = data.upper()
+f2.write(convertedData)
+f2.close()
---
+++ PIP/something.py
@@ -0,0 +1,12 @@
+# def func(a,b,**kwargs) :
+#     print(type(a))
+#     print(type(b))
+#     print(type(kwargs))
+
+# func(1,2,x:10,y:20)
+
+
+tup = (1,2,3,4)
+tup[0]= 5
+print(tup)
+// gives error
