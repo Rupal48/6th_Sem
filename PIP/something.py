6f4a2fd43e5900ad34d9b987df9b8791f755a5a2
Rupal Malhotra <rupalmalhotra4803@gmail.com>
2024-06-10 18:42:34 +0530

    blah

--- PIP/something.py
+++ PIP/something.py
@@ -6,7 +6,16 @@
 # func(1,2,x:10,y:20)
 
 
-tup = (1,2,3,4)
-tup[0]= 5
-print(tup)
-// gives error
+# tup = (1,2,3,4)
+# tup[0]= 5
+# print(tup)
+# gives error
+
+# s = {1,2,3,4,1,2}
+# for x in s:
+#     print(x,end=" ")
+
+dict = {1:2,2:4}
+
+for value in dict.values() :
+    print(value)
